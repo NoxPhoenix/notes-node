303035d9fac3b18cba178b9bf3d3c9f34d929805
NoxPhoenix <mr.mikyo@gmail.com>
2017-02-23 13:22:32 -0500

    1st commit of notes infrastructure
    Added yargs and methods to append files.

--- notes.js
+++ notes.js
@@ -1,6 +1,23 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+const fs = require('fs');
+
+
+// addNote method creates a txt file, and appends the yargs title and body to
+// the document.
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+  fs.writeFile(`${title}.txt`, body, (err) => {
+    if (err) throw err;
+  console.log('Note added');
+});
+};
+
+var getAll = () => {
+  fs.
+}
+
+module.exports = {
+  addNote;
+
 };
